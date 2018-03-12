c189235d1f0a2381125c1eed2dff05d8c5229bb1
Paul Severe <p.james.oconnor@gmail.com>
2018-03-12 17:45:20 -0400

    init

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+class MyComponent extends React.Component {
+  render() {
+    return (
+      <div>This is my component</div>
+    );
+  }
+}
+export default MyComponent;
